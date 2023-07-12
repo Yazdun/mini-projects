35a0d5a7e5f3d9337333fed6513bb104c58b2248
Yazdun <yazduntube@gmail.com>
2023-07-13 03:16:54 +0430

    feat: create card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/public/background.jpg')",
+      },
+    },
   },
   plugins: [],
 };
