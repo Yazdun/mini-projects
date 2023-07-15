c99d2592e357e36638a2740aa203b0cde4b167eb
Yazdun <yazduntube@gmail.com>
2023-07-15 05:31:30 +0430

    feat: create hero component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-desktop": "url('/desktop.jpg')",
+        "hero-mobile": "url('/mobile.jpg')",
+      },
+    },
   },
   plugins: [],
 };
