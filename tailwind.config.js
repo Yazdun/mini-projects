c80291af20233a2e47978b9d8d434370039b4dcb
Yazdun <yazduntube@gmail.com>
2023-07-20 01:39:10 +0330

    create hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        desktop: "url('/desktop.jpg')",
+        tablet: "url('/tablet.jpg')",
+        mobile: "url('/mobile.jpg')",
+      },
+    },
   },
   plugins: [],
 };
