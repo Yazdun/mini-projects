eddf00f592bb5e8ecf7c2717ed27fd5dba23befc
Yazdun <yazduntube@gmail.com>
2023-07-20 03:42:54 +0330

    improve responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      boxShadow: {
+        "5xl": "rgba(0,0,0,1) 0px 22px 70px 40px",
+      },
       backgroundImage: {
         desktop: "url('/desktop.jpg')",
         tablet: "url('/tablet.jpg')",
