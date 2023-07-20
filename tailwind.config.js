b36d14b8228be89aaef8cb34efd87937549568f5
Yazdun <yazduntube@gmail.com>
2023-07-21 00:47:18 +0330

    create component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      boxShadow: {
-        "5xl": "rgba(0, 0, 0, 0.56) 0px 22px 70px 4px",
+      colors: {
+        primary: "#cbcdcc",
       },
     },
   },
